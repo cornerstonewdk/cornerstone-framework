75da5ecb15583e840aaca5304fc2591ffd72b730
jonghyun <nmj21c@prandy.com>
2012-09-04 11:10:06 +0900

    라우트 이름 추가

--- examples/tetris-together/app.js
+++ examples/tetris-together/app.js
@@ -49,6 +49,8 @@ app.configure('development', function(){
 
 app.get('/', routes.index);
 app.get('/watchRoom', routes.watchRoom);
+app.get('/t-tris', routes.index);
+app.get('/t-tris/watchRoom', routes.watchRoom);
 
 var server = http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
