d1e2134beef0bef0a774ad368ff849a615a77d27
yong hoo shin <yhshin@prandy.com>
2012-09-13 13:51:02 +0900

    test-jsonp 추가 ( jquery, requirejs 버전 )

---
+++ test/test-jsonp/jsonp.jquery.js
@@ -0,0 +1,38 @@
+;( function ( $, window ) {
+	
+	// 전역객체로 jsonp 선언
+	var Jsonp = window.Jsonp = {};
+	
+	// get 함수 정의
+	Jsonp.get = function ( options ) {
+
+		// 기본설정
+		var defaults = {
+			cache: false,
+			data: undefined,
+			dataType: 'jsonp',
+			jsonp: 'callback',
+			url: undefined,
+			timeout: 1000 * 3,
+			success: function ( data ) {
+				alert( 'result data : ' + data );
+			},
+			error: function ( jqXHR, textStatus, errorThrown ) {
+	      		alert( 'error : ' + errorThrown.statusText );
+			}
+		}
+
+		// 사용자가 넘겨준 options를 defaults와 병합하여 opt를 생성한다.
+		var opt = $.extend( defaults, options );
+
+		// options.callback 이 존재할 경우 jquery 형식으로 응답받을 callback
+		// 함수명을 설정해준다.
+		if( options.callback )
+			opt.jsonpCallback = options.callback;
+
+		// 새로 생성한 opt을 가지고 ajax 요청을 수행한다.
+		$.ajax( opt );
+
+	};
+	
+} )( jQuery, window );
