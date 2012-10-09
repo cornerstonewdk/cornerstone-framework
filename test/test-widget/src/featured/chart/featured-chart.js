11c394163271630ce00083b087c38ab227b83731
김우섭 <azamara@prandy.com>
2012-10-10 02:48:50 +0900

    # 차트 NVD3 적용 업데이트 및 각 차트별 JSON 포맷 업데이트

--- test/test-widget/src/featured/chart/featured-chart.js
+++ test/test-widget/src/featured/chart/featured-chart.js
@@ -18,9 +18,9 @@
         factory(root.jQuery, root, doc, root.d3);
     }
 }(this, document, function ($, window, document, d3, undefined) {
-
-    var featuredChart,
-        _options = {
+    var pluginName = "featuredChart",
+        featuredChart,
+        defaultOptions = {
             chartType:"bar",
             lineType:"basis",
             width:960,
@@ -34,315 +34,162 @@
 
     FeaturedChart.name = 'FeaturedChart';
 
-    function FeaturedChart(options) {
+    function FeaturedChart(element, options) {
+        var self = this;
+        var target = d3.select(element);
+        this.options = options = $.extend(true, defaultOptions, options);
+        this.$el = $(element);
+
+        this[options.chartType + "Chart"](target, options);
 
         // 배열로 넘어온 색상을 d3 색상 카테고리로 변환
-        if(typeof options.color === "object" && options.color.length > 0) {
+        if (typeof options.color === "object" && options.color.length > 0) {
             options.color = d3.scale.ordinal().range(options.color);
         }
-
-        this.options = $.extend({}, _options, options);
     }
 
-    FeaturedChart.prototype.createChart = function (el) {
-        var chartSize, matrix, widthSize, heightSize, width, height, target = d3.select(el), self = this,
-            chartType = this.options.chartType;
+    FeaturedChart.prototype.barChart = function (target, options) {
+        nv.addGraph(function () {
+            var chart = nv.models.multiBarChart();
 
-        if (chartType === "line") {
-            chartSize = this.lineChart(target);
-        } else if (chartType === "bar") {
-            chartSize = this.barChart(target);
-        } else if (chartType === "pie") {
-            chartSize = this.pieChart(target);
-        } else {
-            return false;
-        }
+            chart.xAxis
+                .tickFormat(d3.format(',f'));
 
-        this.updateChart(el, chartSize);
+            chart.yAxis
+                .tickFormat(d3.format(',.1f'));
 
-//        $(window).on("resize", el, function () {
-//            self.updateChart(el, chartSize);
-//        });
-    };
+            target.append("svg:svg")
+                .datum(options.data)
+                .transition().duration(500).call(chart);
 
-    FeaturedChart.prototype.updateChart = function (el, chartSize) {
-        var width, widthSize, height, heightSize, matrix = getComputedStyle(el)[this.getCssVendorPropertyName(el, "Transform")];
-        if (typeof matrix === "string" && matrix === "none") {
-            matrix = "matrix(1, 0, 0, 1, 0, 0)";
-        }
-        matrix = eval(matrix.replace("matrix(", "[").replace(")", "]"));
-        width = chartSize.width;
-        widthSize = matrix[0];
-        width = width * widthSize;
-        height = chartSize.height;
-        heightSize = matrix[3];
-        height = height * heightSize;
-        $(el).parent().css({
-            width:width,
-            height:height
+            nv.utils.windowResize(chart.update);
+
+            return chart;
         });
     };
 
-    FeaturedChart.prototype.getCssVendorPropertyName = function (target, prop) {
-        var prefixes = ['Moz', 'Webkit', 'O', 'ms'];
-        var prop_ = prop.charAt(0).toUpperCase() + prop.substr(1);
+    FeaturedChart.prototype.lineChart = function (target, options) {
+        // Wrapping in nv.addGraph allows for '0 timeout render', stors rendered charts in nv.graphs, and may do more in the future... it's NOT required
+        nv.addGraph(function () {
+            var chart = nv.models.lineChart();
 
-        if (prop in target.style) {
-            return prop;
-        }
+            chart.xAxis// chart sub-models (ie. xAxis, yAxis, etc) when accessed directly, return themselves, not the partent chart, so need to chain separately
+                .tickFormat(d3.format(',r'));
 
-        for (var i = 0; i < prefixes.length; ++i) {
-            var vendorProp = prefixes[i] + prop_;
-            if (vendorProp in target.style) {
-                return vendorProp;
-            }
-        }
+            chart.yAxis
+                .axisLabel('Voltage (v)')
+                .tickFormat(d3.format(',.2f'));
+
+            target.append("svg:svg")
+                .datum(options.data)
+                .transition().duration(500)
+                .call(chart);
+
+            nv.utils.windowResize(chart.update);
+            return chart;
+        });
     };
 
-    FeaturedChart.prototype.barChart = function (target) {
-        var valueCount, arrayValue = [],
-            self = this,
-            m = self.options.margin,
-            p = self.options.padding,
-            w = self.options.width - m[1] - m[3],
-            h = self.options.height - m[0] - m[2],
-            x = d3.scale.ordinal().rangeRoundBands([0, w - p[1] - p[3]]),
-            y = d3.scale.linear().range([0, h - p[0] - p[2]]),
-            z = self.options.color,
-            data = self.options.data;
+    FeaturedChart.prototype.pieChart = function (target, options) {
+        nv.addGraph(function () {
+            var width = 500;
+            var height = 500;
+            var fitScreen = false;
+            var zoom = 1;
 
-        target = target.append("svg:svg")
-            .attr("width", w)
-            .attr("height", h)
-            .append("svg:g")
-            .attr("transform", "translate(" + p[3] + "," + (h - p[2]) + ")");
+            var chart = nv.models.pieChart()
+                .x(function (d) {
+                    return d.label
+                })
+                .y(function (d) {
+                    return d.value
+                })
+                //.showLabels(false)
+                .color(d3.scale.category10().range())
+                .width(width)
+                .height(height);
 
-        // JSON 키 / 값의 개수 얻기
-        function getJsonKeyLenght(obj) {
-            var count = 0;
-            for (var prop in obj) {
-                if (obj.hasOwnProperty(prop)) {
-                    ++count;
+            target.append("svg:svg")
+                .datum(options.data)
+                .transition().duration(1200)
+                .attr('width', width)
+                .attr('height', height)
+                .call(chart);
+
+            setChartViewBox();
+            resizeChart(target);
+            nv.utils.windowResize(resizeChart);
+
+            function resizeChart(target) {
+                var svg = target.select('svg');
+
+                if (fitScreen) {
+                    // resize based on container's width AND HEIGHT
+                    var windowSize = nv.utils.windowSize();
+                    svg.attr("width", windowSize.width);
+                    svg.attr("height", windowSize.height);
+                } else {
+                    // resize based on container's width
+                    var aspect = chart.width() / chart.height();
+                    var targetWidth = parseInt(target.style('width'));
+                    svg.attr("width", targetWidth);
+                    svg.attr("height", Math.round(targetWidth / aspect));
                 }
             }
-            return count;
-        }
 
-        // VALUE 개수 구하기
-        valueCount = getJsonKeyLenght(data[0]) - 1;
-        for (var i = 0; i < valueCount; i++) {
-            arrayValue.push("value" + i);
-        }
-        // 스택 레이아웃 구조로 데이터를 가공
-        var causes = d3.layout.stack()(arrayValue.map(function (cause) {
-            return data.map(function (d) {
-                return {x:d.label, y:+d[cause]};
-            });
-        }));
+            function setChartViewBox() {
+                var w = width * zoom,
+                    h = height * zoom;
 
-        // X 축 값 설정
-        x.domain(causes[0].map(function (d) {
-            return d.x;
-        }));
-        // Y 축 값 설정
-        y.domain([0, d3.max(causes[causes.length - 1], function (d) {
-            return d.y;
-        })]);
+                chart
+                    .width(w)
+                    .height(h);
 
-        // BAR를 그릴때 스타일링
-        var cause = target.selectAll("g.cause")
-            .data(causes)
-            .enter().append("svg:g")
-            .attr("class", "cause")
-            .style("fill", function (d, i) {
-                return z(i);
-            })
-            .style("stroke", function (d, i) {
-                return d3.rgb(z(i)).darker();
-            });
+                target.select('svg')
+                    .attr('viewBox', '0 0 ' + w + ' ' + h)
+                    .transition().duration(500)
+                    .call(chart);
+            }
 
-        // Bar가 Y축으로 올라갈때 애니매이션 연출
-        var rect = cause.selectAll("rect")
-            .data(Object)
-            .enter().append("svg:rect")
-            .attr("x", function (d) {
-                return x(d.x);
-            })
-            .attr("width", x.rangeBand())
-            .transition().delay(function (d, i) {
-                return i * 100;
-            })
-            .duration(100)
-            .attr("y", function (d) {
-                return -y(d.y0) - y(d.y);
-            })
-            .attr("height", function (d) {
-                return y(d.y);
-            });
-
-        // X, Y축 스타일 지정 및 이름 설정
-        var label = target.selectAll("text")
-            .data(x.domain())
-            .enter().append("svg:text")
-            .attr("class", "x")
-            .attr("x", function (d) {
-                return x(d) + x.rangeBand() / 2;
-            })
-            .attr("y", 6)
-            .attr("text-anchor", "middle")
-            .attr("dy", ".71em")
-            .style("fill", "#fff")
-            .text(function (d) {
-                return d;
-            });
-
-        var rule = target.selectAll("g.rule")
-            .data(y.ticks(5))
-            .enter().append("svg:g")
-            .attr("class", "rule")
-            .style("fill", "#fff")
-            .attr("transform", function (d) {
-                return "translate(0," + -y(d) + ")";
-            });
-
-        rule.append("svg:line")
-            .attr("x2", w - p[1] - p[3])
-            .style("stroke", function (d) {
-                return d ? "#fff" : "#000";
-            })
-            .style("stroke-opacity", function (d) {
-                return d ? .7 : null;
-            });
-
-        rule.append("svg:text")
-            .attr("x", w - p[1] - p[3] + 6)
-            .attr("dy", ".35em")
-            .text(d3.format(",d"));
-
-        return {width:self.options.width, height:self.options.height};
+            return chart;
+        });
     };
 
-    FeaturedChart.prototype.lineChart = function (target) {
-        var self = this,
-            m = self.options.margin,
-            p = self.options.padding,
-            w = self.options.width - m[1] - m[3],
-            h = self.options.height - m[0] - m[2],
-            data = self.options.data;
+    FeaturedChart.prototype.scatterChart = function (target, options) {
+        nv.addGraph(function () {
+            var chart = nv.models.scatterChart()
+                .showDistX(true)
+                .showDistY(true)
+                .color(d3.scale.category10().range());
 
-        var tempData = [];
-        $.map(data, function (val, i) {
-            tempData.push(parseInt(val.value0));
+            chart.xAxis.tickFormat(d3.format('.02f'))
+            chart.yAxis.tickFormat(d3.format('.02f'))
+
+            target.append("svg:svg")
+                .datum(options.data)
+                .transition().duration(500)
+                .call(chart);
+
+            nv.utils.windowResize(chart.update);
+
+            return chart;
         });
-
-        // X scale will fit all values from data[] within pixels 0-w
-        var x = d3.scale.linear().domain([0, tempData.length]).range([0, w]);
-        var y = d3.scale.linear().domain([0, d3.max(tempData)]).range([h, 0]);
-
-        // create a line function that can convert data[] into x and y points
-        var line = d3.svg.line()
-            .x(function (d, i) {
-                return x(i);
-            })
-            .y(function (d) {
-                return y(d);
-            }).interpolate(self.options.lineType);
-
-        var graph = target.append("svg:svg")
-            .attr("width", w + m[1] + m[3])
-            .attr("height", h + m[0] + m[2])
-            .append("svg:g")
-            .attr("transform", "translate(" + m[3] + "," + m[0] + ")");
-
-        graph.append("svg:path").attr("d", line(tempData))
-            .attr("transform", "translate(-25)");
-
-        // X 축 생성
-        var xAxis = d3.svg.axis().scale(x).tickSize(-h).tickSubdivide(true);
-
-        // X 축 추가
-        graph.append("svg:g")
-            .attr("class", "x axis")
-            .attr("transform", "translate(0," + h + ")")
-            .call(xAxis)
-            .selectAll("text")
-            .attr("class","text")
-            .text(function (d) {
-                return data[d].label;
-            });
-
-        // Y 축 생성
-        var yAxisLeft = d3.svg.axis().scale(y).ticks(4).orient("left");
-
-        // Y 축 추가
-        graph.append("svg:g")
-            .attr("class", "y axis")
-            .attr("transform", "translate(-25,0)")
-            .call(yAxisLeft)
-            .selectAll("text")
-            .attr("class","text");
-
-        return {width:self.options.width, height:self.options.height};
-    };
-
-    FeaturedChart.prototype.pieChart = function (target) {
-        var self = this,
-            w = self.options.width,
-            h = self.options.height,
-            r = Math.min(w, h) / 2,
-            color = self.options.color,
-            donut = d3.layout.pie().sort(null),
-            data = self.options.data;
-
-        /**
-         * 1. target 엘리먼트에 svg 태그를 생성
-         * 2. data 바인딩
-         * 3. 폭, 높이값 지정
-         */
-        var vis = target = target.append("svg:svg")
-            .data([data])
-            .attr("width", w)
-            .attr("height", h)
-            .append("svg:g")
-            .attr("transform", "translate(" + r + "," + r + ")");
-
-        var arc = d3.svg.arc().innerRadius(0).outerRadius(r);
-
-        var pie = d3.layout.pie().value(function (d) {
-            return d.value0;
-        });
-
-        var arcs = vis.selectAll("g.slice")
-            .data(pie)
-            .enter()
-            .append("svg:g")
-            .attr("class", "slice");
-
-        arcs.append("svg:path")
-            .attr("fill", function (d, i) {
-                return color(i);
-            })
-            .attr("d", arc);
-
-        arcs.append("svg:text")
-            .attr("transform", function (d) {
-                d.innerRadius = 0;
-                d.outerRadius = r;
-                return "translate(" + arc.centroid(d) + ")";
-            })
-            .attr("text-anchor", "middle")
-            .attr("class", "label")
-            .text(function (d, i) {
-                return data[i].label;
-            });
-
-        return {width:this.options.width, height:this.options.height};
     };
 
     $.fn.featuredChart = function (options) {
         return this.each(function (i) {
-            featuredChart = new FeaturedChart(options);
-            featuredChart.createChart(this);
+            var $this = $(this);
+            var data = $this.data(pluginName);
+
+            // 초기 실행된 경우 플러그인을 해당 엘리먼트에 data 등록
+            if (!data) {
+                $this.data(pluginName, (data = new FeaturedChart(this, options)))
+            }
+
+            // 옵션이 문자로 넘어온 경우 함수를 실행시키도록 한다.
+            if (typeof options == 'string') {
+                data[options](data.options);
+            }
         });
     };
 
@@ -355,15 +202,11 @@
         var self = this,
             dataUrl = $(this).data("chartBind");
 
-        $.getJSON(dataUrl).success(function (msg) {
-            featuredChart = new FeaturedChart({
-                margin:$(self).data("chartMargin"),
-                padding:$(self).data("chartPadding"),
-                chartType:$(self).data("chartType"),
-                lineType:$(self).data("chartListtype"),
-                data:msg
+        $.getJSON(dataUrl).success(function (json) {
+            var featuredChart = new FeaturedChart();
+            featuredChart[$(self).data("chartType") + "Chart"](d3.select(self), {
+                data:json
             });
-            featuredChart.createChart(self);
         }).error(function (jqXHR, textStatus, errorThrown) {
                 console.log("getJSON Error", jqXHR, textStatus, errorThrown);
             });
