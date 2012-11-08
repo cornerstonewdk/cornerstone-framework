b96594956a0995d8af0a23865d0c06f8520cdf89
손혁진 <hjson@prandy.com>
2012-11-08 16:08:53 +0900

    test-event: 이미지 크기를 변경하는 것으로 수정

--- test/test-event/js/view/gesture-area.js
+++ test/test-event/js/view/gesture-area.js
@@ -6,12 +6,12 @@ define( [ 'gesture-view' ], function( GestureView ) {
 		el: 'div#gesture-area',
 
 		initialize: function() {
-			this.$obj = this.$( 'div#gesture-obj' );
+			this.$obj = this.$( 'img#image' );
 			this.currentDegree = 0;
 		},
 
 		events: {
-			'doubletap div#gesture-obj': 'doubletap',
+			'doubletap': 'doubletap',
 			'dragstart': 'dragstart',
 			'drag': 'drag',
 			'dragend': 'dragend',
@@ -20,32 +20,21 @@ define( [ 'gesture-view' ], function( GestureView ) {
 			'transformend': 'transformend'
 		},
 		
-		// 이벤트가 발생한 클릭(터치) 위치가 #gesture-obj 위인지를 검사한다.	
-		isOnGestureObj: function( position ) {
-		
-			var objPosition = this.$obj.position();
-			
-			if ( position.x < objPosition.left ) return false;
-			if ( position.y < objPosition.top ) return false;
-			if ( position.x > objPosition.left + this.$obj.width() ) return false;
-			if ( position.y > objPosition.top + this.$obj.height() ) return false;
-			
-			return true;
-		},
-		
 		doubletap: function( event ) {
-			this.changeFontSize( 1.2 );
+			this.$obj.css( {
+				width: this.$obj.width() * 1.2,
+				height: this.$obj.height() * 1.2
+			} );
 		},
 		
 		dragstart: function( event ) {
+			event.preventDefault();
 		
-			if ( this.isOnGestureObj( event.position ) ) {
-				this.position = this.$obj.position();
-				
-				// position()으로 구한 위치는 padding(19px)을 포함하고 있으므로 제외한다.
-				this.position.left -= 19;
-				this.position.top -= 19;
-			}
+			this.position = this.$obj.position();
+			
+			// position()으로 구한 위치는 padding(19px)을 포함하고 있으므로 제외한다.
+			this.position.left -= 19;
+			this.position.top -= 19;
 		},
 		
 		drag: function( event ) {
@@ -57,11 +46,6 @@ define( [ 'gesture-view' ], function( GestureView ) {
 				top: this.position.top + event.distanceY
 			};
 			
-			if ( newPosition.left < 0 ) newPosition.left = 0;
-			if ( newPosition.top < 0 ) newPosition.top = 0;
-			if ( newPosition.left > this.$el.width() - this.$obj.width() ) newPosition.left = this.$el.width() - this.$obj.width();
-			if ( newPosition.top > this.$el.height() - this.$obj.height() ) newPosition.top = this.$el.height() - this.$obj.height();
-			
 			this.$obj.css( newPosition );
 		},
 		
@@ -70,27 +54,23 @@ define( [ 'gesture-view' ], function( GestureView ) {
 		},
 		
 		transformstart: function( event ) {
-			this.fontSize = parseInt( this.$obj.css( 'font-size' ) );
+			event.preventDefault();
+			
+			this.width = this.$obj.width();
+			this.height = this.$obj.height();
 			this.degree = this.currentDegree;
 		},
 		
 		transform: function( event ) {
-			this.$obj.css( 'font-size', this.fontSize * event.scale );
-			this.$obj.css( '-webkit-transform', 'rotate(' + ( this.degree + event.rotation ) + 'deg)' );
+			this.$obj.css( {
+				width: this.width * event.scale,
+				height: this.height * event.scale,
+				'-webkit-transform': 'rotate(' + ( this.degree + event.rotation ) + 'deg)'
+			} );
 		},
 		
 		transformend: function( event ) {
 			this.currentDegree = this.degree + event.rotation;
-		},
-		
-		changeFontSize: function( times ) {
-			var fontSize = parseInt( this.$obj.css( 'font-size' ) );
-			this.$obj.css( 'font-size', fontSize * times );
-		},
-		
-		rotate: function( degree ) {
-			this.currentDegree += degree;
-			this.$obj.css( '-webkit-transform', 'rotate(' + this.currentDegree + 'deg)' );
 		}
 	} );
 } );
