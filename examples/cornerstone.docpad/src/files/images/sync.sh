cef26db7b507dde185643bd908ad8317e6d6bf90
appadmin <appadmin@appadmin@SKT-HTMLDWEB1>
2012-11-20 11:06:13 +0900

    livedoc

--- examples/cornerstone.docpad/src/files/images/sync.sh
+++ examples/cornerstone.docpad/src/files/images/sync.sh
@@ -1,3 +1,4 @@
 cp "/home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.doc/doc/1. User_Document/images"/* ./
 cp "/home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.doc/doc/2. Reference/images"/* ./
+cp "/home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.doc/doc/3. Runtime/images"/* ./
 
