8f7da0c57db6aff74e3968c7f2ce2e2c3aabd405
손혁진 <hjson@prandy.com>
2012-12-07 15:59:05 +0900

    test-event -> test-gesture로 변경, 테스트 페이지에서 소스 코드 확인가능하게 수정

--- test/test-gesture/main.js
+++ test/test-gesture/main.js
@@ -3,7 +3,7 @@
  * main.js
  * 애플리케이션 메인
  */
-define( [ 'view/gesture-area', 'backbone', 'bootstrap' ], function( GestureAreaView, Backbone ) {
+define( [ 'gesture-area', 'backbone', 'bootstrap' ], function( GestureAreaView, Backbone ) {
 	return {
 		launch: function() {
 			new GestureAreaView();
