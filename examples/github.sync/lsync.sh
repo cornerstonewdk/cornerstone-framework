7d79a4741712ea3c0399f74c65e8ed5bf103036c
appadmin <appadmin@appadmin@SKT-HTMLDWEB1>
2013-01-30 11:01:46 +0900

    라이브독 소스정리

--- examples/github.sync/lsync.sh
+++ examples/github.sync/lsync.sh
@@ -2,20 +2,26 @@
 
 mv *.eco eco_bak/
 
-#java Repl /home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.doc/doc/1* 2> LOG1
-#java Repl /home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.doc/doc/2* 2> LOG2
-#java Repl /home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.doc/doc/3* 2> LOG3
+CLASSPATH=.:./json_library/json-lib-2.3-jdk15.jar:./json_library/commons-lang-2.6.jar:./json_library/ezmorph-1.0.6.jar:./json_library/commons-logging-1.1.1.jar:./json_library/commons-collections-3.2.1.jar:./json_library/commons-beanutils-1.8.3.jar
 
-java -classpath .:./json_library/json-lib-2.3-jdk15.jar:./json_library/commons-lang-2.6.jar:./json_library/ezmorph-1.0.6.jar:./json_library/commons-logging-1.1.1.jar:./json_library/commons-collections-3.2.1.jar:./json_library/commons-beanutils-1.8.3.jar Repl cornerdoc/1* 2> LOG1
-java -classpath .:./json_library/json-lib-2.3-jdk15.jar:./json_library/commons-lang-2.6.jar:./json_library/ezmorph-1.0.6.jar:./json_library/commons-logging-1.1.1.jar:./json_library/commons-collections-3.2.1.jar:./json_library/commons-beanutils-1.8.3.jar Repl cornerdoc/2* 2> LOG2
-java -classpath .:./json_library/json-lib-2.3-jdk15.jar:./json_library/commons-lang-2.6.jar:./json_library/ezmorph-1.0.6.jar:./json_library/commons-logging-1.1.1.jar:./json_library/commons-collections-3.2.1.jar:./json_library/commons-beanutils-1.8.3.jar Repl cornerdoc/3* 2> LOG3
+DEV=/home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.dev
+DEV_DOC=$DEV/doc
 
-rm -rf /home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.dev/examples/cornerstone.livedoc/src/documents/*.eco
-cp *.eco /home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.dev/examples/cornerstone.livedoc/src/documents/
+java -classpath $CLASSPATH Repl $DEV_DOC/1* 2> LOG1
+java -classpath $CLASSPATH Repl $DEV_DOC/2* 2> LOG2
+java -classpath $CLASSPATH Repl $DEV_DOC/3* 2> LOG3
 
-LIVEDOC_IMG=/home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.dev/examples/cornerstone.livedoc/src/files/images
-cp "/home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.dev/doc/1. User_Document/images"/* $LIVEDOC_IMG/
-cp "/home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.dev/doc/2. Reference/images"/* $LIVEDOC_IMG/
-cp "/home/appadmin/CORNER/git/CornersGithub/cornerstone-framework.dev/doc/3. Runtime/images"/* $LIVEDOC_IMG/
+DEV_EXAMPLES=$DEV/examples
+LIVEDOC=$DEV_EXAMPLES/cornerstone.livedoc
+
+LIVEDOC_DOCUMENTS=$LIVEDOC/src/documents
+
+rm -rf $LIVEDOC_DOCUMENTS/*.eco
+cp *.eco $LIVEDOC_DOCUMENTS/
+
+LIVEDOC_FILES_IMAGES=$LIVEDOC/src/files/images
+cp $DEV_DOC/"1. User_Document/images"/*  $LIVEDOC_FILES_IMAGES/
+cp $DEV_DOC/"2. Reference/images"/*      $LIVEDOC_FILES_IMAGES/
+cp $DEV_DOC/"3. Runtime/images"/*        $LIVEDOC_FILES_IMAGES/
 
 
