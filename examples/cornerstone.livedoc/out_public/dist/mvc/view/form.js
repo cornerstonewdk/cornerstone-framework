842f02f393e061c2c1a748b0a8bbb1634e69ce3c
정현철 <atclones@prandy.com>
2013-03-04 16:51:14 +0900

    livdoc 소스정리

---
+++ examples/cornerstone.livedoc/out_public/dist/mvc/view/form.js
@@ -0,0 +1,7 @@
+/*
+    Cornerstone Framework v0.9.1
+
+    COPYRIGHT(C) 2012 BY SKTELECOM CO., LTD. ALL RIGHTS RESERVED.
+    Released under the Apache License, Version 2.0
+*/
+define(["backbone","underscore","jquery","validation-view","bootstrap"],function(e,t,n,r){return e.View.extend({initialize:function(){this.render(),this.model.on("change",this.render,this),this.options.validationViewClass?this.validation=new this.options.validationViewClass({el:this.$el}):this.validation=new r({el:this.$el})},render:function(){var e=this;t.each(this.model.attributes,function(n,r){var i=e.$(":input[name="+r+"]:first");if(!i||!i.length)return;var s=i.attr("type");s&&s.toUpperCase()==="CHECKBOX"?(e.$(":input[name="+r+"]").removeAttr("checked"),t.isArray(n)?t.each(n,function(t,n){e.$(":input[name="+r+"][value="+t+"]").attr("checked","checked")}):e.$(":input[name="+r+"][value="+n+"]").attr("checked","checked")):s&&s.toUpperCase()==="RADIO"?e.$(":input[name="+r+"][value="+n+"]").attr("checked","checked"):i.val(n)})},_onValidationError:function(e,n){t.isArray(n)?t.each(n,this.validation.fail):this.validation.fail(n)},toModel:function(){this.validation.reset();var e={};return t.each(this.$el.serializeArray(),function(n,r){e[n.name]?(t.isArray(e[n.name])||(e[n.name]=[e[n.name]]),e[n.name].push(n.value)):e[n.name]=n.value}),this.model.off("error",this._onValidationError,this),this.model.on("error",this._onValidationError,this),this.model.clear({silent:!0}),this.model.set(e),this.model.isValid()&&this.validation.success(),this.model}})})
---
+++ examples/cornerstone.livedoc/out_public/dist/mvc/view/gesture.js
@@ -0,0 +1,7 @@
+/*
+    Cornerstone Framework v0.9.1
+
+    COPYRIGHT(C) 2012 BY SKTELECOM CO., LTD. ALL RIGHTS RESERVED.
+    Released under the Apache License, Version 2.0
+*/
+define(["backbone","underscore","jquery","jquery.hammer"],function(e,t,n){return e.View.extend({constructor:function(r,i){var s=this.gestureOptions,o=n(this.el),u={};this.events&&t.each(this.events,function(e,n){var r=n.match(/^(\S+)\s*(.*)$/),i=r[1],s=r[2];t.contains(["hold","tap","doubletap","transformstart","transform","transformend","dragstart","drag","dragend","swipe","release"],i)&&(u[s]?u[s].push(i):u[s]=[i])}),t.each(u,function(e,n){var r=s?t.clone(s):{};r.drag=t.contains(e,"dragstart")||t.contains(e,"drag")||t.contains(e,"dragend"),r.swipe=t.contains(e,"swipe"),r.transform=t.contains(e,"transformstart")||t.contains(e,"transform")||t.contains(e,"transformend"),r.tap=t.contains(e,"tap"),r.tap_double=t.contains(e,"doubletap"),r.hold=t.contains(e,"hold"),(n?o.find(n):o).hammer(r)}),e.View.apply(this,arguments)}})})
