4ec589baf6b950ec6aa7629bd7276e486d561da4
김우섭 <azamara@prandy.com>
2013-06-24 17:01:47 +0900

    UI 테스트 페이지 업데이트

--- test/test-cornerstone-ui/js/main.js
+++ test/test-cornerstone-ui/js/main.js
@@ -1,6 +1,6 @@
 (function ($, window, document, undefined) {
 
-	$(function() {
+	$(function () {
 		// 설정되지 않은 링크 기능 방지
 		$("a[href='#'], [type='submit']").on("click", function (e) {
 			e.preventDefault();
@@ -30,7 +30,9 @@
 		$("[data-toggle=tooltip]").tooltip();
 		$("[data-toggle=popover]").popover();
 
-		// RangeInput 스크립트로 적응
+		//
+		// RangeInput 플러그인
+		// --------------------------------------------------
 		$("#range3").rangeinput({"inputShow": true});
 
 		//
@@ -42,7 +44,7 @@
 		$("button.show-sign").on('click', function (e) {
 			var data = $("#signature").sign("getData", "image"); // Base64 형태의 이미지 데이터 리턴
 			$("div.widget-sign-viewer").html($("<img/>", {
-				src:"data:" + data
+				src: "data:" + data
 			}));
 			console.log(data);
 		});
@@ -51,6 +53,44 @@
 		$("button.reset-sign").on('click', function (e) {
 			$("#signature").sign("reset"); // 초기화
 			$(".widget-sign-viewer img").remove();
+		});
+
+		//
+		// Spinner 플러그인
+		// --------------------------------------------------
+		$("[data-plugin='spinner']").on("click", function (e) {
+			e.preventDefault();
+			var target = $(this).data("spinnerTarget");
+			window.setTimeout(function () {
+				$(target).spinner("hide");
+			}, 3000);
+		});
+
+		//
+		// Motion Captcha 플러그인
+		// --------------------------------------------------
+		$("#motion-captcha button").on("click", function (e) {
+			$("#mc-canvas").remove();
+			$("<canvas/>", {
+				"id": "mc-canvas",
+				"class": "mc-canvas"
+			}).appendTo($("#motion-captcha-example")).motioncaptcha({
+					errorMsg: '다시 시도해주세요.',
+					successMsg: '성공',
+					onSuccess: function () {
+						console.log("성공");
+					}
+				});
+		});
+		$("#motion-captcha button").trigger("click");
+
+		//
+		// Date Picker 플러그인
+		// --------------------------------------------------
+		$('#date-picker1, #date-picker2').datepicker({
+			language:"kr",
+			firstDisable: true,
+			changeDisplay: true
 		});
 
 
