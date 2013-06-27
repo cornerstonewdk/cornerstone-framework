28f8f45beeb3e18601e8f1f6d7f65dd25a09bff7
김우섭 <azamara@prandy.com>
2013-06-27 14:48:56 +0900

    cornerstone-pinterest 데모 예제 추가

---
+++ examples/cornerstone-pinterest/scripts/main.js
@@ -0,0 +1,72 @@
+require.config({
+	paths: {
+		jquery: "../bower_components/jquery/jquery",
+		bootstrap: "vendor/bootstrap",
+		jqueryPlugins: "vendor/plugins",
+
+		transition: "vendor/transition"
+	},
+
+	shim: {
+		bootstrap: {
+			deps: ["jquery", "jqueryPlugins"],
+			exports: "jquery"
+		},
+		jqueryPlugins: ["jquery"]
+	}
+});
+
+define(["router", "handlebars", "bootstrap"], function (Router, Handlebars) {
+	"use strict";
+
+	var width = 236;
+	var isEach = false;
+	var isEachReverse = false;
+
+	Handlebars.registerHelper('each', function (context, options) {
+		var result = "";
+		var body = context.data.body;
+
+		for (var i = 0; i < body.length; i++) {
+			if (!body[i].index) {
+				body[i].index = i;
+			}
+
+			if (!isEachReverse) {
+				body[i].height = body[i].height * (context.currentImageWidth / width);
+			}
+
+			result = result + options.fn(body[i]);
+		}
+
+		isEach = true;
+
+		return result;
+	});
+
+	Handlebars.registerHelper('eachReverse', function (context, options) {
+		var result = "";
+		var body = context.data.body;
+
+		for (var i = body.length - 1; i >= 0; i--) {
+			if (!body[i].index) {
+				body[i].index = i;
+			}
+
+			if (!isEach) {
+				body[i].height = body[i].height * (context.currentImageWidth / width);
+			}
+
+			isEachReverse = true;
+			result = result + options.fn(body[i]);
+		}
+
+		return result;
+	});
+
+	return {
+		launch: function () {
+			console.log("Start application!");
+		}
+	}
+});
