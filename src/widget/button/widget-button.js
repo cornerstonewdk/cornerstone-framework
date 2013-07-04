686fd343723910d3ae1dfc550dba156d7fbdab53
yong hoo shin <yhshin@prandy.com>
2013-07-04 17:25:07 +0900

    bs button toggle 이벤트 확장

---
+++ src/widget/button/widget-button.js
@@ -0,0 +1,44 @@
+;
+( function ( root, doc, factory ) {
+    if ( typeof define === "function" && define.amd ) {
+        // AMD
+        define( [ 'backbone', 'underscore', 'jquery' ], function ( Backbone, _, $ ) {
+            factory( $, root, doc );
+            return Backbone.View.extend( {
+                tagName: 'button',
+                className: 'btn',
+                render: function () {
+                    this.$el.attr( 'data-toggle', 'button' );
+                    return this;
+                }
+            } );
+        } );
+    } else {
+        // None AMD
+        factory( root.jQuery, root, doc );
+    }
+} ( window, document, function ( $, window, document ) {
+    /*
+     토글 버튼 이벤트 확장 : 토글시 이벤트 발생 ( single 이외에 다른 토글 버튼도 모두 적용 ? );
+     */
+    var Button = function () {};
+
+    Button = $.fn.button.Constructor;
+
+    Button.prototype.toggle = function () {
+        var $parent = this.$element.closest('[data-toggle="buttons-radio"]');
+        
+        if ($parent) {
+            $parent.find('.active').removeClass('active')
+        }
+        this.$element.toggleClass('active');
+        if ( this.$element.hasClass( 'active' ) ) {
+            this.$element.trigger( e = $.Event('toggleOn') );
+        } else {
+            this.$element.trigger( e = $.Event('toggleOff') );
+        }
+
+    };
+
+    $.fn.button.Constructor = Button;
+} ) );
