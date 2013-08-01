ee3b5092067bfc3f8ab51144888cf3f936a39040
손혁진 <hjson@prandy.com>
2013-08-01 17:45:29 +0900

    Cornerstone CLI: AMD를 사용하지 않는 Scaffolding도 MultipageRouter를 사용해서 구현

--- tools/cornerstone-cli/src/template/noamd/src/scripts/main.js
+++ tools/cornerstone-cli/src/template/noamd/src/scripts/main.js
@@ -3,3 +3,40 @@
  * main.js
  * 애플리케이션 메인
  */
+
+$( '#page1' ).on( 'render', function( event ) {
+	$( this ).html(
+		'<div class="panel panel-primary">' +
+		'	<div class="panel-heading">Page 1</div>' +
+		'	<p>첫번째 페이지 입니다.</p>' +
+		'	<div class="btn-group">' +
+		'		<a href="#page2" class="btn btn-success" data-transition="slide">다음</a>' +
+		'	</div>' +
+		'</div>'
+	);
+} );
+
+$( '#page2' ).on( 'render', function( event ) {
+	$( this ).html(
+		'<div class="panel panel-primary">' +
+		'	<div class="panel-heading">Page 2</div>' +
+		'	<p>두번째 페이지 입니다.</p>' +
+		'	<div class="btn-group">' +
+		'		<a href="#page1" class="btn btn-success">이전</a>' +
+		'		<a href="#page3" class="btn btn-success" data-transition="slide">다음</a>' +
+		'	</div>' +
+		'</div>'
+	);
+} );
+
+$( '#page3' ).on( 'render', function( event ) {
+	$( this ).html(
+		'<div class="panel panel-primary">' +
+		'	<div class="panel-heading">Page 3</div>' +
+		'	<p>세번째 페이지 입니다.</p>' +
+		'	<div class="btn-group">' +
+		'		<a href="#page2" class="btn btn-success">이전</a>' +
+		'	</div>' +
+		'</div>'
+	);
+} );
