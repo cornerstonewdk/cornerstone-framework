9ee5c511e43bd1efdaaa71e72544848c8efdbeaa
손혁진 <hjson@prandy.com>
2013-08-21 14:28:55 +0900

    Cornerstone CLI: Bootstrap 3 정식릴리즈에 맞게 scaffolding 수정

--- tools/cornerstone-cli/src/template/noamd/src/scripts/main.js
+++ tools/cornerstone-cli/src/template/noamd/src/scripts/main.js
@@ -8,9 +8,11 @@ $( '#page1' ).on( 'render', function( event ) {
 	$( this ).html(
 		'<div class="panel panel-primary">' +
 		'	<div class="panel-heading">Page 1</div>' +
-		'	<p>첫번째 페이지 입니다.</p>' +
-		'	<div class="btn-group">' +
-		'		<a href="#page2" class="btn btn-success" data-transition="slide">다음</a>' +
+		'	<div class="panel-body">' +
+		'		<p>첫번째 페이지 입니다.</p>' +
+		'		<div class="btn-group">' +
+		'			<a href="#page2" class="btn btn-success" data-transition="slide">다음</a>' +
+		'		</div>' +
 		'	</div>' +
 		'</div>'
 	);
@@ -20,10 +22,12 @@ $( '#page2' ).on( 'render', function( event ) {
 	$( this ).html(
 		'<div class="panel panel-primary">' +
 		'	<div class="panel-heading">Page 2</div>' +
-		'	<p>두번째 페이지 입니다.</p>' +
-		'	<div class="btn-group">' +
-		'		<a href="#page1" class="btn btn-success">이전</a>' +
-		'		<a href="#page3" class="btn btn-success" data-transition="slide">다음</a>' +
+		'	<div class="panel-body">' +
+		'		<p>두번째 페이지 입니다.</p>' +
+		'		<div class="btn-group">' +
+		'			<a href="#page1" class="btn btn-success">이전</a>' +
+		'			<a href="#page3" class="btn btn-success" data-transition="slide">다음</a>' +
+		'		</div>' +
 		'	</div>' +
 		'</div>'
 	);
@@ -33,9 +37,11 @@ $( '#page3' ).on( 'render', function( event ) {
 	$( this ).html(
 		'<div class="panel panel-primary">' +
 		'	<div class="panel-heading">Page 3</div>' +
-		'	<p>세번째 페이지 입니다.</p>' +
-		'	<div class="btn-group">' +
-		'		<a href="#page2" class="btn btn-success">이전</a>' +
+		'	<div class="panel-body">' +
+		'		<p>세번째 페이지 입니다.</p>' +
+		'		<div class="btn-group">' +
+		'			<a href="#page2" class="btn btn-success">이전</a>' +
+		'		</div>' +
 		'	</div>' +
 		'</div>'
 	);
