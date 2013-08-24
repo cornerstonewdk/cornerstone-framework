1b77ac7daecd6b9d9609ad0ae7075feb46239c11
김우섭 <azamara@prandy.com>
2013-08-25 02:02:56 +0900

    Theme Breadcrumbs, Labels, Typography Less 통합

--- test/test-cornerstone-ui/src/app/main.js
+++ test/test-cornerstone-ui/src/app/main.js
@@ -2,34 +2,59 @@
  * main.js
  * 애플리케이션 메인
  */
-define([ 'view/page1', 'view/page2', 'backbone', 'bootstrap', 'style!main' ], function (Page1View, Page2View, Backbone) {
-	return {
-		launch: function () {
+define([
+    'view/page1',
+    'view/page2',
+    'backbone',
+    'multipage-router',
+    'bootstrap',
+    'style!main' ], function (Page1View, Page2View, Backbone, MultipageRouter) {
+    return {
+        launch: function () {
 
-			// Router
-			var MainRouter = Backbone.Router.extend({
-				routes: {
-					'': "page1",
-					':id': "page2"
-				},
-				page1: function() {
-					console.log(1);
-					var view = new Page1View();
-					view.render();
-				},
-				page2: function(id) {
-					console.log(id);
-					var view = new Page2View();
-					view.render(id);
-				}
-			});
+            // Router
+            var MainRouter = MultipageRouter.extend({
+                pages: {
+                    'page1': {
+                        fragment: [ '', 'page1' ],
+                        el: '#page1',
+                        render: function () {
+                            new Page1View().render();
+                        },
+                        active: function () {
+                            this.releasePage('#page1');
+                        }
+                    },
+                    'page2': {
+                        fragment: [ ':id', 'page2' ],
+                        el: '#page2',
+                        render: function (id) {
+                            new Page2View().render(id);
+                        },
+                        active: function () {
+                            this.releasePage('#page2');
+                        }
+                    }
+                },
+                transitions: {
+                    'page1:page2': 'slide'
+                },
 
-			$(document).on("click", ".navbar:not(body > .navbar)", function(e) {
-				e.preventDefault();
-			});
+                releasePage: function (currentPage) {
+                    return $('.container > section:not(' + currentPage + ')').html('');
+                }
+            });
 
-			new MainRouter();
-			Backbone.history.start();
-		}
-	};
+            $(document).on('click', '#page2 a', function (e) {
+                e.preventDefault();
+            });
+
+            $('#nav-component .dropdown-menu a').on('click', function () {
+                $(this).closest('.dropdown').find('.dropdown-toggle').dropdown('toggle');
+            });
+
+            new MainRouter();
+            Backbone.history.start();
+        }
+    };
 });
