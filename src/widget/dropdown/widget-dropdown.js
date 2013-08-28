6fa4faa2ecade902ea2973e4e69697f795c1cf83
yhshin <yhshin@prandy.com>
2013-08-28 14:02:51 +0900

    render 함수 내용 수정

--- src/widget/dropdown/widget-dropdown.js
+++ src/widget/dropdown/widget-dropdown.js
@@ -6,7 +6,7 @@
             factory( $, root, doc );
             return Backbone.View.extend( {
             	render: function () {
-            		this.$el.collapse( this.options );
+            		this.$el.dropdown( this.options );
             		return this;
             	}
             } );
@@ -15,44 +15,4 @@
         // None AMD
         factory( root.jQuery, root, doc );
     }
-} ( window, document, function ( $, window, document ) {
-	var HAS_TOUCH = ('ontouchstart' in window);
-
-	/**
-     * Collapse
-     */
-    var Collapse = $.fn.collapse.Constructor;
-
-    // 터치기반에 최적화된 Show 함수
-    if(HAS_TOUCH) {
-        Collapse.prototype.toggle =  function () {
-            this.$element[0].style["WebkitTransition"] = "none";
-            this[this.$element.hasClass('in') ? 'hide' : 'show']()
-			// console.log("in");
-			// this.$element.hasClass('in') ? this.$element.removeClass('in') : this.$element.addClass('in');
-        };
-
-        Collapse.prototype.transition = function (method, startEvent, completeEvent) {
-            var that = this
-                , complete = function () {
-                    if (startEvent.type == 'show') that.reset()
-                    that.transitioning = 0
-                    that.$element.trigger(completeEvent)
-                }
-
-            this.$element.trigger(startEvent)
-
-            if (startEvent.isDefaultPrevented()) return
-
-            this.transitioning = 1
-
-            this.$element[method]('in')
-			// Duration
-			// $.support.transition && this.$element.hasClass('collapse') ?
-			// this.$element.one($.support.transition.end, complete) :
-            complete()
-        };
-    }
-    $.fn.collapse.Constructor = Collapse;
-
-} ) );
+} ( window, document, function ( $, window, document ) {} ) );
