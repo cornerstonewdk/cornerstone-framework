0c93bff990cbe5cb873c6471096dfece9bd33790
yonghoo shin <pieces49@naver.com>
2013-08-30 11:29:16 +0900

    widget-modal 테스트 코드 보강

--- test/test-bdd/spec/spec.js
+++ test/test-bdd/spec/spec.js
@@ -495,29 +495,48 @@ describe('Cornerstone event extend test', function() {
     //     });
     // });
 
-    describe('widget-media', function() {
-        var media;
+    // describe('widget-media', function() {
+    //     var media;
+
+    //     it('requirejs를 이용하여 모듈로 로드하고, Backbone.View의 인스턴스여야 한다.', function(done) {
+    //         require(['widget-media'],function(WidgetMedia){
+    //             var MediaModel = Backbone.Model.extend({
+    //                 alwaysShowControls: true
+    //             }); 
+    //             media = new WidgetMedia();
+    //             $('#test').html(media.render());
+    //             expect(media).to.be.an.instanceof(Backbone.View);
+    //             done();
+    //         });
+    //     });
+    // });
+
+    describe('widget-modal', function() {
+        var modal;
 
         it('requirejs를 이용하여 모듈로 로드하고, Backbone.View의 인스턴스여야 한다.', function(done) {
-            require(['widget-media'],function(WidgetMedia){
-                var MediaModel = Backbone.Model.extend({
-                    alwaysShowControls: true
-                }); 
-                media = new WidgetMedia();
-                $('#test').html(media.render());
-                expect(media).to.be.an.instanceof(Backbone.View);
+            require(['widget-modal'],function(WidgetModal){
+                modal = new WidgetModal({
+                    el: '#myModal'
+                });
+                modal.render();
+                expect(modal).to.be.an.instanceof(Backbone.View);
+                $('#btnModalToggle').click(function(){
+                    modal.$el.modal('toggle');
+                });
                 done();
             });
         });
+
+        it('modal이 적용된 후 토글을 실행하였을 때 modal이 사라져야 한다.',function(done){
+            modal.$el.modal('toggle');
+            this.timeout(2000);
+            setTimeout(function() {
+                expect(modal.$el.data('bs.modal').$backdrop).to.be.equal(null);
+                done()
+            }, 500);
+        });
     });
-
-    // describe('widget-modal', function() {
-    //     var modal;
-
-    //     it('requirejs를 이용하여 모듈로 로드하고, Backbone.View의 인스턴스여야 한다.', function() {
-
-    //     });
-    // });
 
     // describe('widget-motioncaptcha', function() {
     //     var captcha;
