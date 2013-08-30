6657edbc91a0d448aa2cf1c5249ef74cd9bf6a53
yonghoo shin <pieces49@naver.com>
2013-08-30 14:02:10 +0900

    widget-tab 추가

---
+++ src/widget/tab/widget-tab.js
@@ -0,0 +1,18 @@
+;
+(function(root, doc, factory) {
+    if (typeof define === "function" && define.amd) {
+        // AMD
+        define(['backbone', 'underscore', 'jquery', 'bootstrap'], function(Backbone, _, $) {
+            factory($, root, doc);
+            return Backbone.View.extend({
+                render: function() {
+                    this.$el.tab(this.options);
+                    return this;
+                }
+            });
+        });
+    } else {
+        // None AMD
+        factory(root.jQuery, root, doc);
+    }
+}(window, document, function($, window, document) {}));
