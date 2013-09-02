552da75accd7703a3a80aa76579f64c688c99c7f
김우섭 <azamara@prandy.com>
2013-09-02 16:06:37 +0900

    라인포커스 차트 추가

--- src/widget/chart/widget-chart.js
+++ src/widget/chart/widget-chart.js
@@ -30,6 +30,11 @@
             this.$el.data("currentChart", chart);
             this.$el.data("currentChartControl", options.control);
 
+            var groups = target.selectAll(".nv-groups");
+            $(groups[0]).each(function() {
+                $(this).parent().attr("clip-path", "");
+            });
+
             this.util.applyBindEvent(target, options, chart, this.$el);
         },
         render: function (options) {
@@ -246,61 +251,13 @@
             return chart;
         },
         lineFocusChart: function (target) {
-            self = this;
-            function data() {
-                return stream_layers(3, 10 + Math.random() * 200, .1).map(function (data, i) {
-                    return {
-                        key: "Stream" + i,
-                        values: data
-                    };
-                });
-            }
+            chart = nv.models.lineWithFocusChart();
 
-            /* Inspired by Lee Byron"s test data generator. */
-            function stream_layers(n, m, o) {
-                if (arguments.length < 3) o = 0;
-                function bump(a) {
-                    var x = 1 / (.1 + Math.random()),
-                        y = 2 * Math.random() - .5,
-                        z = 10 / (.1 + Math.random());
-                    for (var i = 0; i < m; i++) {
-                        var w = (i / m - y) * z;
-                        a[i] += x * Math.exp(-w * w);
-                    }
-                }
+            chart.xAxis.tickFormat(d3.format(",f"));
+            chart.yAxis.tickFormat(d3.format(",.2f"));
+            chart.y2Axis.tickFormat(d3.format(",.2f"));
 
-                return d3.range(n).map(function () {
-                    var a = [], i;
-                    for (i = 0; i < m; i++) a[i] = o + o * Math.random();
-                    for (i = 0; i < 5; i++) bump(a);
-                    return a.map(stream_index);
-                });
-            }
-
-            function stream_index(d, i) {
-                return {x: i, y: Math.max(0, d)};
-            }
-
-            nv.addGraph(function () {
-                chart = nv.models.lineWithFocusChart();
-
-                chart.xAxis
-                    .tickFormat(d3.format(",f"));
-
-                chart.yAxis
-                    .tickFormat(d3.format(",.2f"));
-
-                chart.y2Axis
-                    .tickFormat(d3.format(",.2f"));
-
-                target = self.util.getTarget(target, options);
-
-                target.datum(data())
-                    .transition().duration(500)
-                    .call(chart);
-
-                return chart;
-            });
+            return chart;
         },
         bar3dChart: function (target, options) {
             var self = this;
@@ -684,7 +641,7 @@
                         resizeChart();
                     });
                 } else {
-                    chart.dispatch.on("stateChange", function (e) {
+                    chart.dispatch.stateChange && chart.dispatch.on("stateChange", function (e) {
 
                         isDebug && console.log("New State:", JSON.stringify(e));
 
@@ -702,8 +659,6 @@
                     });
 
                     !options.autoResize || nv.utils.windowResize(function () {
-
-
                         chart.update();
                         setTimeout(function () {
                             typeof chart.afterRender === "function" && chart.afterRender("resize");
