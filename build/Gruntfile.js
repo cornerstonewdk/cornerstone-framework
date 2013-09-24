9a3b6206c4178986479a019366da356b2ea72ad7
yhshin <yhshin@prandy.com>
2013-09-24 16:32:46 +0900

    grunt 단일 페키지 업로드 tast 추가 사용법 grunt publish:페키지명 다 올릴경우 기존과 동일

--- build/Gruntfile.js
+++ build/Gruntfile.js
@@ -164,6 +164,42 @@ module.exports = function ( grunt ) {
 		}
 	} );
 
+	grunt.registerTask( 'up', function ( arg1, arg2 ) {
+		var done = this.async();
+		var dirPath;
+
+		if ( arguments.length === 0 ) {
+			grunt.log.writeln('Error : Need path info of package.json');
+			return false;
+		} 
+
+		dirPath = arg1 + ':/' + arg2;
+		
+		grunt.file.recurse( dirPath, function callback( abspath, rootdir, subdir, filename ) { 
+			if ( filename == 'package.json' ) {
+
+				var child = cp.exec( 'jam publish ' + rootdir + ' --force --repository ' + pathInfo.repo);
+
+				child.stdin.setEncoding( 'utf-8' );
+				child.stdout.pipe( process.stdout );
+
+				child.stdout.on( 'data', function( chunk ) {
+
+					if ( chunk.substr( -10 ) == 'Username: ' )
+						child.stdin.write( admin.id + '\n' );
+					else if ( chunk.substr( -10 ) == 'Password: ' )
+						child.stdin.write( admin.pass + '\n' );
+					else {
+						setTimeout( function () {
+							done();	
+						}, 1000 );
+						
+					}
+				} );
+			}
+		} );
+	} );
+
 	grunt.registerTask( 'findPackages', function () {
 		grunt.file.recurse( pathInfo.dist, function callback( abspath, rootdir, subdir, filename ) { 
 			if ( filename == 'package.json' ) packages.push( rootdir + subdir );
@@ -203,7 +239,40 @@ module.exports = function ( grunt ) {
 		);
 	} );
 
-	grunt.registerTask( 'publish', [ 'findPackages', 'upload' ] );
+	grunt.registerTask( 'publish', function ( arg1 ) {
+		if ( arguments.length === 0 ) 
+			grunt.task.run( [ 'findPackages', 'upload' ] );	
+		else {
+			
+			var done = this.async();
+			grunt.file.recurse( pathInfo.dist, function callback( abspath, rootdir, subdir, filename ) { 
+				if ( filename == 'package.json' ) {
+					
+					var loaded = grunt.file.readJSON( abspath );
+					
+					if ( loaded.name === arg1 ) {
+
+						var child = cp.exec( 'jam publish ' + rootdir + subdir + ' --force --repository ' + pathInfo.repo );
+
+						child.stdin.setEncoding( 'utf-8' );
+						child.stdout.pipe( process.stdout );
+
+						child.stdout.on( 'data', function( chunk ) {
+							if ( chunk.substr( -10 ) == 'Username: ' )
+								child.stdin.write( admin.id + '\n' );
+							else if ( chunk.substr( -10 ) == 'Password: ' )
+								child.stdin.write( admin.pass + '\n' );
+						} );
+
+						setTimeout( function () {
+							done();
+						}, 1000 );
+					}
+				} 
+			} );
+		}
+	} );
+
 	grunt.registerTask( 'build', [ 'clean', 'copy', 'uglify', 'less', 'cssmin' ] );
 	grunt.registerTask( 'default', [ 'build', 'publish' ] );
 }
