c4c7358b797dd19b5c1a397c4e709344739c773b
김우섭 <azamara@prandy.com>
2013-10-08 11:50:10 +0900

    Widget Collapse BackboneView 글로벌에 추가하는 기능 구현 및 코드 리팩토링

--- src/widget/collapse/widget-collapse.js
+++ src/widget/collapse/widget-collapse.js
@@ -1,58 +1,55 @@
-;
-( function ( root, doc, factory ) {
-    if ( typeof define === "function" && define.amd ) {
-        // AMD
-        define( [ 'backbone', 'underscore', 'jquery', 'bootstrap' ], function ( Backbone, _, $ ) {
-            factory( $, root, doc );
-            return Backbone.View.extend( {
-            	render: function () {
-            		this.$el.collapse( this.options );
-            		return this;
-            	}
-            } );
-        } );
-    } else {
-        // None AMD
-        factory( root.jQuery, root, doc );
-    }
-} ( window, document, function ( $, window, document ) {
-	var HAS_TOUCH = ('ontouchstart' in window);
+;(function (root, factory) {
 
-	/**
+    // Require.js가 있을 경우
+    if (typeof define === 'function' && define.amd)
+        define([ "jquery", "underscore", "backbone", "bootstrap" ], factory);
+    else
+        root.Collapse = factory(root.$, root._, root.Backbone);
+
+}(window, function ($, _, Backbone) {
+    var HAS_TOUCH = ('ontouchstart' in window);
+
+    /**
      * Collapse
      */
     var Collapse = $.fn.collapse.Constructor;
 
     // 터치기반에 최적화된 Show 함수
-    if(HAS_TOUCH) {
-        Collapse.prototype.toggle =  function () {
+    if (HAS_TOUCH) {
+        Collapse.prototype.toggle = function () {
             this.$element[0].style["WebkitTransition"] = "none";
-            this[this.$element.hasClass('in') ? 'hide' : 'show']()
-			// console.log("in");
-			// this.$element.hasClass('in') ? this.$element.removeClass('in') : this.$element.addClass('in');
+            this[this.$element.hasClass('in') ? 'hide' : 'show']();
+            // console.log("in");
+            // this.$element.hasClass('in') ? this.$element.removeClass('in') : this.$element.addClass('in');
         };
 
         Collapse.prototype.transition = function (method, startEvent, completeEvent) {
             var that = this
-                , complete = function () {
-                    if (startEvent.type == 'show') that.reset()
-                    that.transitioning = 0
-                    that.$element.trigger(completeEvent)
-                }
+            , complete = function () {
+                if (startEvent.type == 'show') that.reset();
+                that.transitioning = 0;
+                that.$element.trigger(completeEvent)
+            };
 
-            this.$element.trigger(startEvent)
+            this.$element.trigger(startEvent);
 
-            if (startEvent.isDefaultPrevented()) return
+            if (startEvent.isDefaultPrevented()) return;
 
-            this.transitioning = 1
+            this.transitioning = 1;
 
-            this.$element[method]('in')
-			// Duration
-			// $.support.transition && this.$element.hasClass('collapse') ?
-			// this.$element.one($.support.transition.end, complete) :
+            this.$element[method]('in');
+            // Duration
+            // $.support.transition && this.$element.hasClass('collapse') ?
+            // this.$element.one($.support.transition.end, complete) :
             complete()
         };
     }
     $.fn.collapse.Constructor = Collapse;
 
-} ) );
+    return Backbone ? Backbone.View.extend({
+        render: function () {
+            this.$el.collapse(this.options);
+            return this;
+        }
+    }) : Collapse;
+}));
