d348e8d75d757097712da658eb5c3294a82b99ff
김우섭 <azamara@prandy.com>
2013-10-09 17:50:37 +0900

    Widget Modal 커버리지 테스트 소스 업데이트

--- test/test-coverage/test/mediaSpec.js
+++ test/test-coverage/test/mediaSpec.js
@@ -4,7 +4,7 @@ define([
     "underscore",
     "backbone",
     "widget-media",
-    "template!test/test-coverage/templates/media",
+    "template!test/test-coverage/templates/media"
 ], function (expect, $, _, Backbone, Media, Template) {
     $("body").append(Template());
 
@@ -12,19 +12,17 @@ define([
         var media1, media2;
 
         it('requirejs를 이용하여 모듈로 로드하고, Backbone.View의 인스턴스여야 한다.', function (done) {
-            require(['widget-media'], function (Media) {
-                media1 = new Media({
-                    el: '#media .panel:first-child video'
-                });
-                media1.render();
-                expect(media1).to.be.an.instanceof(Backbone.View);
-                media2 = new Media({
-                    el: '#media .panel:nth-child(1) audio'
-                });
-                media2.render();
-                expect(media2).to.be.an.instanceof(Backbone.View);
-                done();
+            media1 = new Media({
+                el: '#media .panel:first-child video'
             });
+            media1.render();
+            expect(media1).to.be.an.instanceof(Backbone.View);
+            media2 = new Media({
+                el: '#media .panel:nth-child(1) audio'
+            });
+            media2.render();
+            expect(media2).to.be.an.instanceof(Backbone.View);
+            done();
         });
     });
 });
