31b19f7df00cdaadcea3bc4aa79a7270af28ab81
김우섭 <azamara@prandy.com>
2013-10-09 18:42:46 +0900

    커버리지 테스트 Widget Tab 테스트 코드 업데이트

--- test/test-coverage/test/tabSpec.js
+++ test/test-coverage/test/tabSpec.js
@@ -11,20 +11,18 @@ define([
     describe('widget-tab', function () {
         var tab;
         it('requirejs를 이용하여 모듈로 로드하고, Backbone.View의 인스턴스여야 한다.', function (done) {
-            require(['widget-tab'], function (Tab) {
-                tab = new Tab({
-                    el: '#myTab'
-                });
-                tab.render();
-
-                $('#myTab a').click(function (e) {
-                    e.preventDefault()
-                    $(this).tab('show')
-                })
-
-                expect(tab).to.be.an.instanceof(Backbone.View);
-                done();
+            tab = new Tab({
+                el: '#myTab'
             });
+            tab.render();
+
+            $('#myTab a').click(function (e) {
+                e.preventDefault()
+                $(this).tab('show')
+            })
+
+            expect(tab).to.be.an.instanceof(Backbone.View);
+            done();
         });
 
         it('프로파일 탭을 클릭했을 때 2번째 탭이 보여야 한다.', function (done) {
