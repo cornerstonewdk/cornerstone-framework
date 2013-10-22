524749c82de2b5c2b2e39677491f1917f43c6455
yhshin <yhshin@prandy.com>
2013-10-22 16:58:31 +0900

    테스트 코드 보강중

--- test/test-total/src/app/spec/spec.js
+++ test/test-total/src/app/spec/spec.js
@@ -355,7 +355,7 @@ describe( '종합 테스트', function () {
     var validateUser, validateUser1, validateUser2;
     it( 'form view를 이용하여 model값이 표현되고 model의 값과 표시된 값이 동일한지 확인', function ( done ) {
         require( [ 'template!../app/template/userForm', 'form-view' ], function ( formTamplate, FormView ){
-            validateUser = new UserModel( { name: '트래버', age: 35, job: '노동자', validate:true } );
+            validateUser = new UserModel( { name: '트래버', age: 35, job: '노동자' } );
             pageView.$el.find( '.btn-group' ).next().append( formTamplate() ).find( 'form' ).attr( 'id', 'add-form' );
             formView = new FormView( { el: '#add-form', model: validateUser } );
             expect( formView ).to.be.an.instanceof( Backbone.View );
@@ -378,85 +378,97 @@ describe( '종합 테스트', function () {
             expect( validateUser.get( 'job' ) ).to.be.equal( '스파이' );
             $( this ).off( 'click' );
             done();
-            return false;
         } );
+         formView.$el.find( '.js-submit' ).trigger( 'click' );
+    } );
+
+    // TODO 모델 validation 확인 -> 툴팁 표시 ( validation 2회 발생하는 문제 )
+    it( 'Model 객체에 validate 함수를 구현하고 동작되는지 확인', function ( done ) {
+        formView.$el.find( 'input[name="name"]' ).val( '' );
+
+        formView.$el.find( '.js-submit' ).on( 'click', function () {
+            formView.toModel();
+            var $nextEl = formView.$el.find( 'input[name="name"]' ).next();
+            expect( $nextEl ).to.be.not.undefined;
+            expect( $nextEl.hasClass( 'tooltip' ) ).to.be.true;
+            expect( $nextEl.find( '.tooltip-inner' ).text() ).to.be.equal( '이름을 입력하세요.' );
+            $( this ).off( 'click' );
+            done();
+        } );
+
          formView.$el.find( '.js-submit' ).click();
     } );
 
-    // TODO 모델 validation 확인 -> 툴팁 표시
-    it( 'TODO Model 객체에 validate 함수를 구현하고 동작되는지 확인', function ( done ) {
-        formView.$el.find( 'input[name="name"]' ).val( '' );
-
+    it( 'Model에 invalid 이벤트를 연결하고 model값을 변경했을 때 감지 되는지 확인한다.', function ( done ) {
         validateUser.on( 'invalid', function ( model, error ) {
             expect( model ).to.be.an.instanceof( Backbone.Model );
             expect( error.attribute ).to.be.equal( 'name' );
             done();
         } );
-        
+
         formView.$el.find( '.js-submit' ).on( 'click', function () {
             formView.toModel();
-        } );
-
-         formView.$el.find( '.js-submit' ).click();
+        } ).trigger( 'click' );
     } );
 
-    // TODO input에 data-placement 를 다르게 지정했을 때 툴팁위치가 수정되서 출력되어야 한다.
-    it( 'TODO input에 data-placement 를 다르게 지정했을 때 툴팁위치가 수정되서 출력되어야 한다.', function () {
-    } );
+    // // TODO input에 data-placement 를 다르게 지정했을 때 툴팁위치가 수정되서 출력되어야 한다.
+    // it( 'TODO input에 data-placement 를 다르게 지정했을 때 툴팁위치가 수정되서 출력되어야 한다.', function () {
+    // } );
 
-    // TODO
+    // // TODO
     it( 'TODO validation view를 로드 후 formView에 생성옵션으로 지정, 동작 확인', function ( ) {
         require( [ 'validation-view', 'template!../app/template/userForm', 'form-view' ], function ( ValidationView, formTamplate, FormView ){
             validateUser1 = new UserModel( { name: '프랭클린', age: 25, job: '배송업', validate: true } );
             pageView.$el.find( '#add-form' ).parent().append( formTamplate() ).find( 'form:last-child' ).attr( 'id', 'modify-form' );
             formView1 = new FormView( { el: '#modify-form', model: validateUser1, validationViewClass: ValidationView } );
-            formView1.$el.find( '.js-submit' ).on( 'click', function () {
-                formView1.toModel();
-            } );
+            expect( formView1 ).to.be.an.instanceof( Backbone.View );
+            // formView1.$el.find( '.js-submit' ).on( 'click', function () {
+            //     formView1.toModel();
+            // } );
         } );
     } );
 
-    // TODO
-    it( 'TODO custom validation view를 사용해 본다.', function ( done ) {
-        require( [ 'view/validationView', 'template!../app/template/userForm', 'form-view' ], function ( CustomValidationView, formTamplate, FormView ){
-            validateUser2 = new UserModel( { name: '데빈', age: 40, job: '공무원', validate: true } );
-            pageView.$el.find( '#add-form' ).parent().append( formTamplate() ).find( 'form:last-child' ).attr( 'id', 'valid-form' );
-            formView2 = new FormView( { el: '#valid-form', model: validateUser2, validationViewClass: CustomValidationView } );
-            pageView.$el.find( '#valid-form input[name="name"]' ).val( '' );
-            formView2.$el.find( '.js-submit' ).on( 'click', function () {
-                formView1.toModel();
-                done();
-            } );
-            formView2.$el.find( '.js-submit' ).click();
+    // // TODO
+    // it( 'TODO custom validation view를 사용해 본다.', function ( done ) {
+    //     require( [ 'view/validationView', 'template!../app/template/userForm', 'form-view' ], function ( CustomValidationView, formTamplate, FormView ){
+    //         validateUser2 = new UserModel( { name: '데빈', age: 40, job: '공무원', validate: true } );
+    //         pageView.$el.find( '#add-form' ).parent().append( formTamplate() ).find( 'form:last-child' ).attr( 'id', 'valid-form' );
+    //         formView2 = new FormView( { el: '#valid-form', model: validateUser2, validationViewClass: CustomValidationView } );
+    //         pageView.$el.find( '#valid-form input[name="name"]' ).val( '' );
+    //         formView2.$el.find( '.js-submit' ).on( 'click', function () {
+    //             formView2.toModel();
+    //             done();
+    //         } );
+    //         formView2.$el.find( '.js-submit' ).click();
             
-        } );
-    } );
+    //     } );
+    // } );
 
-    var userCollection;
-    it( 'Collection에 속한 Model이 url을 이어받는지 확인', function (  ) {
-        userCollection = new Users();
-        var usr1 = new UserModel( { id: 1 } ), usr2 = new UserModel( { id: 2 } );
-        userCollection.add( [ usr1, usr2 ] );
-        expect( usr1.url() ).to.be.equal( '/users/1' );
-        expect( usr2.url() ).to.be.equal( '/users/2' );
-    } );
+    // var userCollection;
+    // it( 'Collection에 속한 Model이 url을 이어받는지 확인', function (  ) {
+    //     userCollection = new Users();
+    //     var usr1 = new UserModel( { id: 1 } ), usr2 = new UserModel( { id: 2 } );
+    //     userCollection.add( [ usr1, usr2 ] );
+    //     expect( usr1.url() ).to.be.equal( '/users/1' );
+    //     expect( usr2.url() ).to.be.equal( '/users/2' );
+    // } );
 
-    it( 'Model 클래스를 정의하면서 root url 지정이 되는지 확인', function () {
-        var RootTestUser = Backbone.Model.extend( {
-            urlRoot: '/users'
-        } );
+    // it( 'Model 클래스를 정의하면서 root url 지정이 되는지 확인', function () {
+    //     var RootTestUser = Backbone.Model.extend( {
+    //         urlRoot: '/users'
+    //     } );
 
-        var tempUser = new RootTestUser( { id: 3 } );
-        expect( tempUser.url() ).to.be.equal( '/users/3' )
-    } );
-    // var testUrl = 'https://baas.kinvey.com/appdata/kid_TTuylFz6Oq/users/1';
-    it( 'Model 객체를 생성하면서 root url 지정이 되는지 확인', function () {
-        var RootTestUser1 = Backbone.Model.extend();
-        var tempUser = new RootTestUser1( { id: 100 } );
-        tempUser.urlRoot = 'users/';
-        // expect( tempUser.url() ).to.be.equal( '/users/100' );
-        // tempUser.fetch();
-    } );
+    //     var tempUser = new RootTestUser( { id: 3 } );
+    //     expect( tempUser.url() ).to.be.equal( '/users/3' )
+    // } );
+    // // var testUrl = 'https://baas.kinvey.com/appdata/kid_TTuylFz6Oq/users/1';
+    // it( 'Model 객체를 생성하면서 root url 지정이 되는지 확인', function () {
+    //     var RootTestUser1 = Backbone.Model.extend();
+    //     var tempUser = new RootTestUser1( { id: 100 } );
+    //     tempUser.urlRoot = 'users/';
+    //     // expect( tempUser.url() ).to.be.equal( '/users/100' );
+    //     // tempUser.fetch();
+    // } );
 
     // it( '', function ( done ) {
     //     require( [ '' ], function (  ){
