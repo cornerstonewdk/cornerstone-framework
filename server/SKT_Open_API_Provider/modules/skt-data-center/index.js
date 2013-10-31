ef2d35e464c116c4dde4abe3d23a9dafcbf28f99
yhshin <yhshin@prandy.com>
2013-10-31 16:40:13 +0900

    redirect url 변경

--- server/SKT_Open_API_Provider/modules/skt-data-center/index.js
+++ server/SKT_Open_API_Provider/modules/skt-data-center/index.js
@@ -15,7 +15,7 @@ function SktDataCenter() {
 		'4': {'client_secret': '4secret', 'redirect_uri': 'http://cornerstone.sktelecom.com/cornertest/test-skt-api-client/api-with-conerstone-css/client_redirect.html'},
 		'5': {'client_secret': '5secret', 'redirect_uri': 'http://127.0.0.1:8080/html5/cornerstone-framework/test/test-skt-api-client/api-with-conerstone-css/client_redirect.html'},
 		'6': {'client_secret': '5secret', 'redirect_uri': 'http://61.250.22.139/cornertest/test-skt-api-client/api-with-conerstone-css/'},
-		'7': {'client_secret': '7secret', 'redirect_uri': 'http://cornerstone.sktelecom.com/2/test-util/client_redirect.html'}
+		'7': {'client_secret': '7secret', 'redirect_uri': 'http://cornerstone.sktelecom.com/2/util/client_redirect.html'}
 	};
 	
 	/*
