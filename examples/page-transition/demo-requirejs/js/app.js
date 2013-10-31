cb29409a330bf1f2766a257792cb18ee8f8a50ae
김우섭 <azamara@prandy.com>
2013-10-31 17:03:44 +0900

    page-transition 예제 페이지 Cornerstone Framework 2.0 기준으로 변경

--- examples/page-transition/demo-requirejs/js/app.js
+++ examples/page-transition/demo-requirejs/js/app.js
@@ -2,48 +2,47 @@ define([
     'backbone',
     'jquery',
     'transition'],
-    function (Backbone, $, Transition) {
-        return {
-            init:function () {
-                $("#front .btn").live("click", function (e) {
-                    var transitionType = $(this).attr("data-transition"),
-                        inTargetID = "#back",
-                        outTargetID = "#front";
-                    Transition.launcher({
-                        transitionType:transitionType, // 화면전환 효과 기본 None(효과 없음)
-                        inTarget:{
-                            el:inTargetID // 들어오는 페이지의 element의 셀렉터나 ID 또는 클래스
-                        },
-                        outTarget:{
-                            el:outTargetID // 나가는 페이지의 element의 셀렉터나 ID 또는 클래스
-                        },
-                        isReverse:false, // 뒤로가기 여부
-                        done:function () {
-                            $("#front").removeClass("current");
-                            $("#back").addClass("current");
-                        }
-                    });
+function (Backbone, $, Transition) {
+    return {
+        init: function () {
+            $(document).on("click", "#front .btn", function () {
+                var transitionType = $(this).attr("data-transition"),
+                inTargetID = "#back",
+                outTargetID = "#front";
+                Transition.launcher({
+                    transitionType: transitionType, // 화면전환 효과 기본 None(효과 없음)
+                    inTarget: {
+                        el: inTargetID // 들어오는 페이지의 element의 셀렉터나 ID 또는 클래스
+                    },
+                    outTarget: {
+                        el: outTargetID // 나가는 페이지의 element의 셀렉터나 ID 또는 클래스
+                    },
+                    isReverse: false, // 뒤로가기 여부
+                    done: function () {
+                        $("#front").removeClass("current");
+                        $("#back").addClass("current");
+                    }
                 });
+            });
 
-                $("#back .btn").live("click", function (e) {
-                    var transitionType = $(this).attr("data-transition"),
-                        outTargetID = "#back",
-                        inTargetID = "#front";
-                    Transition.launcher({
-                        inTarget:{
-                            el:inTargetID // 들어오는 페이지의 element의 셀렉터나 ID 또는 클래스
-                        },
-                        outTarget:{
-                            el:outTargetID // 나가는 페이지의 element의 셀렉터나 ID 또는 클래스
-                        },
-                        isReverse:true, // 뒤로가기 여부
-                        done:function () {
-                            $("#front").addClass("current");
-                            $("#back").removeClass("current");
-                        }
-                    });
+            $(document).on("click", "#back .btn", function () {
+                var transitionType = $(this).attr("data-transition"),
+                outTargetID = "#back",
+                inTargetID = "#front";
+                Transition.launcher({
+                    inTarget: {
+                        el: inTargetID // 들어오는 페이지의 element의 셀렉터나 ID 또는 클래스
+                    },
+                    outTarget: {
+                        el: outTargetID // 나가는 페이지의 element의 셀렉터나 ID 또는 클래스
+                    },
+                    isReverse: true, // 뒤로가기 여부
+                    done: function () {
+                        $("#front").addClass("current");
+                        $("#back").removeClass("current");
+                    }
                 });
-            }
+            });
         }
     }
-);
+});
