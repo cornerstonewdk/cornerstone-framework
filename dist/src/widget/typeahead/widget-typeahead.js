3854f40f536b023ab490c9573d7cb7cbc0d1e3c4
김우섭 <azamara@prandy.com>
2013-10-31 14:42:44 +0900

    rebuild

--- dist/src/widget/typeahead/widget-typeahead.js
+++ dist/src/widget/typeahead/widget-typeahead.js
@@ -4,4 +4,4 @@
     COPYRIGHT(C) 2012 BY SKTELECOM CO., LTD. ALL RIGHTS RESERVED.
     Released under the Apache License, Version 2.0
 */
-!function(a,b){"function"==typeof define&&define.amd?define(["jquery","underscore","backbone","typeahead"],b):a.Typeahead=b(a.$,a._,a.Backbone)}(window,function(a,b,c){return a.fn.twitterTypeahead=a.fn.typeahead,a.fn.typeahead=function(b){return this.each(function(){var c=a(this);c.twitterTypeahead(b),c.on("typeahead:selected",function(a,b,d){c.trigger("selected.cs.typeahead",[b,d])})})},c&&c.View.extend({render:function(){return this.$el.typeahead(this.options),this}})});
+!function(a,b){"function"==typeof define&&define.amd?define(["jquery","underscore","backbone","typeahead"],b):a.Typeahead=b(a.$,a._,a.Backbone)}(window,function(a,b,c){return a.fn.twitterTypeahead=a.fn.typeahead,a.fn.typeahead=function(b){return this.each(function(){var c=a(this);c.twitterTypeahead(b),c.on("typeahead:selected",function(a,b,d){c.trigger("selected.cs.typeahead",[b,d])})})},c&&c.View.extend({render:function(a){return"string"==typeof a?this.$el.typeahead(a):this.$el.typeahead(this.options),this}})});
