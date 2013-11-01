6e1dafab2a15543f6f4ddce4ce9c89ec73f3c07d
정현철 <atclones@prandy.com>
2013-11-01 14:32:51 +0900

    livedoc 플로팅 네비게이션 메뉴 추가

--- examples/livedoc/src/app/view/detail-lnb.js
+++ examples/livedoc/src/app/view/detail-lnb.js
@@ -3,6 +3,10 @@ define( [ 'backbone', 'widget-scrollview', 'template!view/detail-lnb' ], functio
 	return Backbone.View.extend( {
 
 		el: '#detail-lnb',
+		events: {
+			"click #page-nav-next": "clickNavNext",
+			"click #page-nav-prev": "clickNavPrev"
+		},
 
 		initialize: function () {
 			var self = this;
@@ -18,7 +22,7 @@ define( [ 'backbone', 'widget-scrollview', 'template!view/detail-lnb' ], functio
 					self.$el.find( '#scrollView' ).css( {
 						height: window.innerHeight / 2
 					} );
-			})
+			});
 		},
 
 		render: function () {
@@ -32,6 +36,39 @@ define( [ 'backbone', 'widget-scrollview', 'template!view/detail-lnb' ], functio
 				this.scrollView = this.$el.find( '#scrollView' ).css( {
 					height: window.innerHeight / 2
 				} ).featuredScrollView().data( 'featuredScrollView' );
+
+			// Comment by JHC, 20131101
+			// 첫 번째 문서인 경우 page-nav-prev (이전문서로 이동하는) 버튼을 제거 한다.
+			if ( this.$el.find( 'a.list-group-item:first' ).hasClass("active") )
+				this.$el.find( '#page-nav-prev' ).remove();
+			// 마지막 문서인 경우 page-nav-next (다음문서로 이동하는) 버튼을 제거 한다.
+			if ( this.$el.find( 'a.list-group-item:last' ).hasClass("active") )
+				this.$el.find( '#page-nav-next' ).remove();
+/*
+			var tocItemList = this.$el.find( 'a.list-group-item' );
+
+			this.$el.find( '#page-nav-prev' ).css( {
+				display: none
+			} );
+
+			if ( tocItemList.length < 2) {
+				this.$el.find( '#page-nav-prev' ).css( {
+					display: none
+				} );
+//				this.$el.find( '#page-nav-prev' ).css( {
+//					display: none;
+//				});
+//				this.$el.find( '#page-nav-next' ).css( {
+//					display: none;
+//				});} 
+
+			else {
+				if ( $( tocItemList[0] ).hasClass("active") )
+					console.log("This Doc is first DOC.");
+				else
+					console.log("This Doc is NOT first DOC.");
+			}
+*/
 
 			return this;
 		},
@@ -52,6 +89,55 @@ define( [ 'backbone', 'widget-scrollview', 'template!view/detail-lnb' ], functio
 				: this.scrollView.iscroll.maxScrollY;
 
 			this.scrollView.iscroll.scrollTo( 0, currentScrollY, 350 );
+
+			// Comment by JHC, 20131101
+			// 첫 번째 문서인 경우 page-nav-prev (이전문서로 이동하는) 버튼이 보이지 않게 한다.
+			if ( this.$el.find( 'a.list-group-item:first' ).hasClass("active") ) {
+				this.$el.find( '.floating-menu-hidden-region' ).append( this.$el.find( '#page-nav-prev' ) );
+			}
+			// 마지막 문서인 경우 page-nav-next (다음문서로 이동하는) 버튼이 보이지 않게 한다.
+			else if ( this.$el.find( 'a.list-group-item:last' ).hasClass("active") ) {
+				this.$el.find( '.floating-menu-hidden-region' ).append( this.$el.find( '#page-nav-next' ) );		
+			}
+			else {
+				this.$el.find( '#page-nav-top' ).before( this.$el.find( '#page-nav-prev' ) );
+				this.$el.find( '#page-nav-top' ).after( this.$el.find( '#page-nav-next' ) );
+			}
+
 		},
+
+		clickNavNext: function (e) {
+			e.preventDefault();
+			// Comment by JHC, 20131031
+			// 목차메뉴의 문서링크를 모두 구한다.
+			var lnbList;
+			lnbList = $( "a.list-group-item" );
+			// 문서링크 중에서 현재 문서의 링크를 찾는다.
+			for (var i=0; i<lnbList.length; i++) {
+
+				if ( $( lnbList[i] ).hasClass("active") )
+					break;
+			}
+			// 목차가 정렬되어 있으므로 현재 문서링크의 바로 다음 것이 다음 문서링크가 된다.
+			// 마지막 문서인 경우 페이지 상에서 'NEXT'버튼을 Disable 하는 것으로 처리 !
+			// 따라서 다음 문서가 없는 경우에 대한 예외처리 코드도 넣지 않음. 
+			location.href = $( lnbList[i+1] ).attr("href");
+		},
+		clickNavPrev: function (e) {
+			// Comment by JHC, 20131031
+			// clickNavNext 와 동일 로직이므로 clickNavNext: 를 참고할 것.
+			e.preventDefault();
+			var lnbList;
+			lnbList = $( "a.list-group-item" );
+			for (var i=0; i<lnbList.length; i++) {
+
+				if ( $( lnbList[i] ).hasClass("active") )
+					break;
+			}
+			// 현재 문서링크의 바로 앞의 것이 이전 문서링크가 된다.
+			// 맨 첫 번째 문서인 경우 페이지 상에서 'PREVIOUS'버튼을 Disable 하는 것으로 처리 !
+			// 따라서 이전 문서가 없는 경우에 대한 예외처리 코드도 넣지 않음. 
+			location.href = $( lnbList[i-1] ).attr("href"); 
+		}
 	} );
 } );
