2542eba4d8d7915abf59ecf2ead186406b4edc2b
김우섭 <azamara@prandy.com>
2013-11-01 13:53:44 +0900

    Livedoc 마크다운 테이블 내의 a 태그는 새창으로 열도록 수정

--- examples/livedoc/src/app/view/detail.js
+++ examples/livedoc/src/app/view/detail.js
@@ -1,27 +1,28 @@
-define( [ 'backbone', 'template!view/detail' ], function ( Backbone, template ) {
+define([ 'backbone', 'template!view/detail' ], function (Backbone, template) {
 
-	return Backbone.View.extend( {
+    return Backbone.View.extend({
 
-		el: '#detail-content',
+        el: '#detail-content',
 
-		initialize: function () {
+        initialize: function () {
 
-		},
+        },
 
-		render: function () {
-			var self = this;
+        render: function () {
+            var self = this;
 
-			// 상세페이지 랜더링
-			this.$el.html( template( { model: this.model.toJSON() } ) );
-			this.customMarkdown();
+            // 상세페이지 랜더링
+            this.$el.html(template({ model: this.model.toJSON() }));
+            this.customMarkdown();
 
-			return this;
-		},
+            return this;
+        },
 
-		customMarkdown: function () {
-			var $docContent = this.$el.find( '.doc-content' );
-			$docContent.find( 'table' ).addClass( 'table table-bordered table-hover table-striped' )
-				.wrap( $( '<div/>', {'class': 'table-responsive'} ) );
-		}
-	} );
-} );
+        customMarkdown: function () {
+            var $docContent = this.$el.find('.doc-content');
+            $docContent.find('table').addClass('table table-bordered table-hover table-striped')
+            .wrap($('<div/>', {'class': 'table-responsive'}))
+            .find('a').attr('target', '_blank');
+        }
+    });
+});
