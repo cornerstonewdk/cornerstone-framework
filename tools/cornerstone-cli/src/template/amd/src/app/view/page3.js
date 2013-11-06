23b73f9510fe942adc58d4a8e89f0afd8b0ffe6f
손혁진 <hjson@prandy.com>
2013-11-06 15:12:56 +0900

    Cornerstone CLI: Scaffolding 소스 코드에 주석 첨부

--- tools/cornerstone-cli/src/template/amd/src/app/view/page3.js
+++ tools/cornerstone-cli/src/template/amd/src/app/view/page3.js
@@ -6,6 +6,7 @@ define( [ 'backbone', 'template!view/page3' ], function( Backbone, template ) {
 		el: 'section#page3',
 
 		render: function() {
+			// page3.template 템플릿을 렌더링한 결과를 el의 하부에 추가한다.
 			this.$el.html( template() );
 			return this;
 		},
